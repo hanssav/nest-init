338d70dcb4f19c614c91baa4035edc43b991855a
hanssav <handihandi40@gmail.com>
2026-02-02 21:07:53 +0700

    fix/auth-module-imports
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [UserModule, PrismaModule],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
